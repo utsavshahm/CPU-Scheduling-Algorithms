edae1af21b4c9cb7b73d0743d1a1cb4c623e1e0a
Utsav Shah <120706939+utsavshahm@users.noreply.github.com>
2024-11-21 15:01:33 +0530

    Delete backend directory

--- backend/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { simulateScheduling } from '../controllers/scheduleController.js';
-
-const router = express.Router();
-
-router.post('/simulate', simulateScheduling);
-
-export default router;
--- backend/utils/analyseData.js
+++
@@ -1,6 +0,0 @@
-export default function analyse(processes) {
-    console.log('PID | Arrival | Burst | Completion | Turnaround');
-    for (const p of processes) {
-        console.log(`${p.pid} | ${p.arrivalTime} | ${p.burstTime} | ${p.completionTime} | ${p.turnaroundTime}`);
-    }
-}
